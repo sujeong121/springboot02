1958c1230c18240332ed831fddff21ef5263aa8c
sujeong121 <hryeo0413@gmail.com>
2023-01-19 14:19:23 +0900

    20230119 session 이 만료되어도 login 유지하기

--- src/main/java/com/global/config/SecurityConfig.java
+++ src/main/java/com/global/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.global.config;
 
+import com.global.account.AccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final AccountService accountService;
+  private final DataSource dataSource;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -31,8 +42,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // logoutSuccessUrl("/") <-- 로그아웃 하면 첫 페이지로 이동함
     http.logout()
         .logoutSuccessUrl("/");
+
+    http.rememberMe()
+            .userDetailsService(accountService)
+            .tokenRepository(tokenRepository());
   }
 
+  @Bean
+  public PersistentTokenRepository tokenRepository(){
+    JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+    jdbcTokenRepository.setDataSource(dataSource);
+    return jdbcTokenRepository;
+  }
   @Override
   public void configure(WebSecurity web) throws Exception {
     // resource/static 에 있는 data 는 Spring Security 로 설정하지 않는 설정
