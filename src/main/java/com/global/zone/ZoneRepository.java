a0815f2109d7f6485409d6f9361783c145500165
sujeong121 <hryeo0413@gmail.com>
2023-01-27 15:10:13 +0900

    20230127 활동 지역 부분 구현 (Zone Entity 초기화하고 ZoneList 에 할당)

---
+++ src/main/java/com/global/zone/ZoneRepository.java
@@ -0,0 +1,8 @@
+package com.global.zone;
+
+import com.global.domain.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
+
+}
